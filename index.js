ef5fcfefbb1c18cb7743a3a450b5089b7c646796
ivymmurithi <ivymurithi@gmail.com>
2021-11-29 22:26:17 +0300

    clear input after submitting

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $("#btn").click(function(){
         let info = $("#input").val();
+        $("#input").val("");
         let display = $("#show-list").append("<li> </li>").append(info);
 
         $("#clear").click(function() {

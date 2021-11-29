71e8fd99662c81888dd337621533dc061b810481
ivymmurithi <ivymurithi@gmail.com>
2021-11-29 22:21:36 +0300

    clear button works

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
     $("#btn").click(function(){
         let info = $("#input").val();
-        $("#show-list").append("<li> </li>").append(info);
+        let display = $("#show-list").append("<li> </li>").append(info);
+
+        $("#clear").click(function() {
+        display.remove();
+        });
     });
 
-    // $("#clear").click(function() {
-
-    // });
 
 });

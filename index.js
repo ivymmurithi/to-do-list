a36d6f19c09f64e976618046043bd599d9a4255d
ivymmurithi <ivymurithi@gmail.com>
2021-11-30 21:36:27 +0300

    add delete button

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ $(document).ready(function() {
     $("#btn").click(function(){
         let info = $("#input").val();
         $("#input").val("");
-        let display = $("#show-list").append("<li> </li>").append(info);
+        let display = $("#show-list").append("<li>" + info + "</li>");
 
         $("#clear").click(function() {
         display.remove();
         });
-    });
 
+        $("<button class = 'deleteBtn'>Delete</button>").appendTo(display);
+
+        $(".deleteBtn").on("click", function(){
+            $(".deleteBtn").parent().children().remove("li, .deleteBtn");
+        });
+
+
+    });
 
 });

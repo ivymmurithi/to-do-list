b31c9738137ed98a061fcf1cfae38b53a1f64f28
ivymmurithi <ivymurithi@gmail.com>
2021-12-04 13:29:14 +0300

    add bulma cdn and background color

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ $(document).ready(function() {
     $("#btn").click(function(){
         let info = $("#input").val();
         $("#input").val("");
-        let display = $("#show-list").append("<li>" + info + "</li>");
-
-        $("#clear").click(function() {
-        display.remove();
-        });
-
-        $("<button class = 'deleteBtn'>Delete</button>").appendTo(display);
-
-        $(".deleteBtn").on("click", function(){
-            $(".deleteBtn").parent().children().remove("li, .deleteBtn");
-        });
-
-
+        let btn = `<button class = "deleteBtn">Delete</button>`;
+        $("#show-list").append("<div><li>" + info + " " + btn + "</li></div>");
     });
+
+    $("#clear").click(function() {
+        $("#show-list").children().remove();
+    });
+
+
+     $("#show-list").on("click", ".deleteBtn", function(){
+            $(this).parent().remove();
+     });
 
 });
